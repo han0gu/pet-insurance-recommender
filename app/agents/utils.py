db4aa54cfaa1a64f0853a0758e25096fb50e909f
han0gu <imgum9@gmail.com>
2026-02-10 21:13:22 +0900

    refactor: graph image 생성 로직을 `utils.py`로 분리

---
+++ app/agents/utils.py
@@ -0,0 +1,23 @@
+import io
+from pathlib import Path
+
+from PIL import Image as PILImage
+
+from langgraph.graph.state import CompiledStateGraph
+
+
+def create_graph_image(
+    graph: CompiledStateGraph,
+    file_name: str,
+    base_dir: str = None,
+):
+    png_data = graph.get_graph().draw_mermaid_png()
+
+    img = PILImage.open(
+        io.BytesIO(png_data)
+    )  # 바이너리 데이터를 메모리 상의 이미지로 변환합니다.
+
+    BASE_DIR = base_dir if base_dir else Path(__file__).resolve().parent
+    img.save(f"{BASE_DIR}/{file_name}.png")
+
+    # img.show()
