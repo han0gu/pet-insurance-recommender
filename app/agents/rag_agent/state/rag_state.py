db7d094ebe348159d4cf03af5b8c0ad07bec0be0
han0gu <imgum9@gmail.com>
2026-02-15 16:35:29 +0900

    feat: vector DB 검색 방식 개선
    - 각 chunk의 메타데이터를 생성할 때 tagger_normal을 사용한 경우와 tagger_simple을 사용한 경우를 구분하여 vector DB collection에 저장
    - 문서 검색 시 하나의 사용자 질문을 기준으로 두 컬렉션에서 동시에 문서를 조회하도록 개선
    - 문서 검색 결과를 평가하는 기능 추가

--- app/agents/rag_agent/state/rag_state.py
+++ app/agents/rag_agent/state/rag_state.py
@@ -5,9 +5,25 @@ from langchain_core.documents import Document
 
 
 class RagState(BaseModel):
-    user_query: Optional[str] | None = None
-    user_query_embedding: Optional[List[float]] = Field(default_factory=list)
-    retrieved_documents: Optional[List[Document]] = Field(default_factory=list)
+    user_query: Optional[str] | None = Field(
+        default=None,
+        description="vector DB 검색 시 query로 사용하기 위해 LLM이 사용자 입력 정보를 바탕으로 생성한 문장",
+    )
+    user_query_embedding: Optional[List[float]] = Field(
+        default_factory=list, description="user_query를 임베딩한 벡터 값"
+    )
+    terms_normal_tag_dense: Optional[List[Document]] = Field(
+        default_factory=list,
+        description="terms_normal_tag_dense 컬렉션에서 검색한 결과",
+    )
+    terms_simple_tag_dense: Optional[List[Document]] = Field(
+        default_factory=list,
+        description="terms_simple_tag_dense 컬렉션에서 검색한 결과",
+    )
+    retrieved_documents: Optional[List[Document]] = Field(
+        default_factory=list,
+        description="평가/정렬 후 최종 취합된 검색 결과",
+    )
 
 
 class GenerateUserQueryOutput(BaseModel):
